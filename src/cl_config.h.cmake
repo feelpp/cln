c0d5c28989afc328b28fe8fdac82a1979e0b14b1
Alexey Sheplyakov <asheplyakov@altlinux.org>
2021-01-02 21:45:28 +0400

    cmake: cl_config.h: avoid redefining GMP_DEMANDS_UINTD_* macros

--- src/cl_config.h.cmake
+++ src/cl_config.h.cmake
@@ -2,12 +2,7 @@
 #define CL_CONFIG_H
 #include "cln/host_cpu.h"
 
-#cmakedefine GMP_DEMANDS_UINTD_INT
-#cmakedefine GMP_DEMANDS_UINTD_LONG
-#cmakedefine GMP_DEMANDS_UINTD_LONG_LONG
-
 #cmakedefine CL_USE_GMP 1
-
 #cmakedefine ASM_UNDERSCORE
 #cmakedefine CL_HAVE_ATTRIBUTE_FLATTEN
 #cmakedefine HAVE_UNISTD_H

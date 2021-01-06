4556f49a15be9ff854f8337e8b9a5dbbcbab2b29
Alexey Sheplyakov <asheplyakov@altlinux.org>
2021-01-07 00:00:32 +0400

    build/CMake: defined `long_long_(big|little)_endian` to intparam.h
    ... for consistency with autotools build.

--- include/cln/intparam.h.cmake
+++ include/cln/intparam.h.cmake
@@ -10,6 +10,8 @@
 #cmakedefine short_little_endian
 #cmakedefine int_little_endian
 #cmakedefine long_little_endian
+#cmakedefine long_long_little_endian
 #cmakedefine short_big_endian
 #cmakedefine int_big_endian
 #cmakedefine long_big_endian
+#cmakedefine long_long_big_endian
